19cc01996410489c78938d16fb4a5e4a7a8f4fc1
lulu1026 <18403928897@163.com>
2025-08-05 18:42:38 +0800

    123

---
+++ 二维数组/创建.cpp
@@ -0,0 +1,17 @@
+//
+// Created by 86176 on 2025/8/5.
+//
+#include "iostream"
+using namespace std;
+int main()
+{
+    int a[2][3] = {1,2,3,4,5,6};
+    for(int i = 0; i < 2; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+}
