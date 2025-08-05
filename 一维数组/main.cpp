d95db390d8aa996ce147a9564536b11068985610
lulu1026 <18403928897@163.com>
2025-08-05 18:43:27 +0800

    123

--- 一维数组/main.cpp
+++ 一维数组/main.cpp
@@ -4,23 +4,24 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a[10] = {8, 6, 4 ,2, 9, 7, 5, 3, 1};
-    cout << "排序前的数组为：" << endl;
-    for(int i = 0; i < 10; i++){
-        cout << a[i] << " ";
+    int a[10] = {8, 6, 4 ,2, 9, 7, 5, 3, 1,0};
+    printf("排序前的数组为：");
+    for(int i = 0; i <= 9; i++){
+        printf("%d ", a[i]);
     }
-    cout << endl << "排序后的数组为：" << endl;
-    for(int i = 0; i < 10; i++){
-        for(int j = i + 1; j < 10; j++){
-            if(a[i] > a[j]){
-                int temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
+    printf("\n");
+    for(int i = 0; i < 10-1; i++){
+        for(int j = 0; j <= 9 - i - 1 ; j++){
+            if(a[j] > a[j+1]){
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
             }
         }
     }
-    for(int i = 0; i < 10; i++){
-        cout << a[i] << " ";
+    printf("排序后的数组为：");
+    for(int i = 0; i <= 9; i++){
+        printf("%d ", a[i]);
     }
     //return 0;
 }
